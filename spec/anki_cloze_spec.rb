c612e58b8ec28e30dca0026e2c3e9f2a93dee93c
Michael Campbell <michael.campbell@gmail.com>
2025-11-19 11:57:51 -0500

    chunk spec

--- spec/anki_cloze_spec.rb
+++ spec/anki_cloze_spec.rb
@@ -23,4 +23,14 @@ describe 'anki-cloze' do
     result = run_anki_cloze('hello   world', '  from   ruby')
     expect(result).to eq('{{c1::hello}} {{c2::world}} {{c3::from}} {{c4::ruby}}')
   end
+
+  it 'groups words into chunks when --chunks is provided' do
+    result = run_anki_cloze('--chunks', '2', 'hello', 'world', 'from', 'ruby')
+    expect(result).to eq('{{c1::hello world}} {{c2::from ruby}}')
+  end
+
+  it 'leaves a final smaller chunk as-is' do
+    result = run_anki_cloze('--chunks', '3', 'one', 'two', 'three', 'four')
+    expect(result).to eq('{{c1::one two three}} {{c2::four}}')
+  end
 end
